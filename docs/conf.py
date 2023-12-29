cdb48ffe2cf590254b32849dedb3dac9afd1719a
Jacob Sánchez <jacobszpz@protonmail.com>
2023-12-29 15:30:31 +0000

    Add hypothesis to dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,6 @@ extensions = ['sphinxcontrib.autodoc_pydantic']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
